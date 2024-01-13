631ee408a71fd1be0ea9412a85966839d709a923
roarigami <roarigami@gmail.com>
2024-01-13 15:25:39 -0500

    Tab one button script

--- Tabs/tabOne/js/script.js
+++ Tabs/tabOne/js/script.js
@@ -8,7 +8,25 @@ $(document).ready(function(){
 
   tabOneBtn.click(function(e) {
 
-    
+        loadScreen.show();
+        setTimeout(function(){
+          userTimeline();
+        },500);
+
+      function userTimeline() {
+        $('#errors').html("");
+        jQuery('#tabTwoContainer').remove();
+        $('#tabOneBox').css("display" , "block");
+        $('#tabTwoBox').css("display" , "none");
+        //reenable the team button
+        tabOneBtn.prop("disabled", false);
+        loadScreen.hide();
+
+        //Have to add index for these to work [0]
+        tabOneBtn[0].classList.add('lsActive');
+        tabTwoBtn[0].classList.remove('lsActive');
+      }
+
     });
 
   tabTwoBtn.click(function(e) {
