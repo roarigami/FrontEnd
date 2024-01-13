0c88dd012a7f5c2651f2437a520b988a40cc8b02
roarigami <roarigami@gmail.com>
2024-01-13 15:23:11 -0500

    Tab two script setup

--- Tabs/tabOne/js/script.js
+++ Tabs/tabOne/js/script.js
@@ -1,3 +1,21 @@
 /*************
 Ajax Tabs Script
 *************/
+$(document).ready(function(){
+
+  let tabOneBtn = $('#tabOneBtn');
+  let tabTwoBtn = $('#tabTwoBtn');
+
+  tabOneBtn.click(function(e) {
+
+    
+    });
+
+  tabTwoBtn.click(function(e) {
+
+
+    });
+
+
+
+});
---
+++ Tabs/tabOne/tabTwo.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+echo '
+<div class="tabTwoContainer" id="tabTwoContainer">
+';
+
+echo '
+
+<h1>Tab Two</h1>
+
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+  illo inventore veritatis et quasi architecto beatae vitae dicta
+  sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+   aut odit aut fugit,</p>
+
+';
+
+
+echo '
+    </div>
+    ';
