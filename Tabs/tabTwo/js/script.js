9ba124bd11b099c05b4d1cf2cbe86a0e7f5ddc23
roarigami <roarigami@gmail.com>
2024-01-14 10:49:11 -0500

    Tab script --functional

--- Tabs/tabTwo/js/script.js
+++ Tabs/tabTwo/js/script.js
@@ -3,10 +3,27 @@ Tab Two Script
 **************/
 
 $( document ).ready(function() {
+  let tabOpen = 0;
+  let btnTabs = document.querySelectorAll('.tab-btn');
+  let tabContent = document.querySelectorAll('.tab-content');
 
-  let tabs = document.querySelectorAll('.tab-btn');
-  let content = document.querySelectorAll('.tab-content');
+  // btnTabs[0].classList.add('.tab-active');
+  for (let i = 0; i < btnTabs.length; i++) {
+    btnTabs[i].addEventListener('click', () => tabActive(i));
+  }
 
+  function tabActive(tabCurrent) {
+    removeActive();
+    btnTabs[tabCurrent].classList.add('tab-active');
+    tabContent[tabCurrent].classList.add('tab-active');
+    console.log(tabCurrent);
+  }
 
-  
+  function removeActive() {
+    for (let i = 0; i < btnTabs.length; i++) {
+      btnTabs[i].classList.remove('tab-active');
+      tabContent[i].classList.remove('tab-active');
+    }
+  }
+
 });
