eb73d18d7bf896ffdb99c6ffbf00119476ef66c6
roarigami <roarigami@gmail.com>
2024-01-22 08:38:01 -0500

    login one script edit

--- Logins/loginOne/js/script.js
+++ Logins/loginOne/js/script.js
@@ -4,13 +4,13 @@ Login One Script
 
 const viewBtn = document.querySelector("[data-password]");
 const password_input = document.getElementById("password");
+const pwdIcon = viewBtn.children[0];
 
 viewBtn.addEventListener("click",function(e){
-
+    pwdIcon.classList.toggle("fa-eye-slash");
     if (password_input.type === "password") {
-    password_input.type = "text";
-  } else if (password_input.type === "text") {
-    password_input.type = "password";
-  }
-
+      password_input.type = "text";
+    } else if (password_input.type === "text") {
+      password_input.type = "password";
+    }
 },false);
