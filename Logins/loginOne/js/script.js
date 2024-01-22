4d46ddcb3ff3f40e58758910cabd1596058246ae
roarigami <roarigami@gmail.com>
2024-01-22 08:32:42 -0500

    login one basic password view script

--- Logins/loginOne/js/script.js
+++ Logins/loginOne/js/script.js
@@ -1,3 +1,16 @@
 /******************
 Login One Script
 *****************/
+
+const viewBtn = document.querySelector("[data-password]");
+const password_input = document.getElementById("password");
+
+viewBtn.addEventListener("click",function(e){
+
+    if (password_input.type === "password") {
+    password_input.type = "text";
+  } else if (password_input.type === "text") {
+    password_input.type = "password";
+  }
+
+},false);
